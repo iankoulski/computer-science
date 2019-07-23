06d460cd51478db9127ed91e5185311c7d3119f5
Alex Iankoulski <alex_iankoulski@yahoo.com>
2019-07-22 19:11:28 -0700

    Remove anagrams examples from Misc playground

--- Container-Root/problems/misc/playground/src/main/java/com/iankoulski/problems/playground/Misc.java
+++ Container-Root/problems/misc/playground/src/main/java/com/iankoulski/problems/playground/Misc.java
@@ -9,60 +9,22 @@ public class Misc
 {
 	static Misc m = new Misc();
 
-    public static void main( String[] args )
-    {
+	public static void main( String[] args )
+	{
 		System.out.println("\n\nvvvvvvvvvvvvvvvvvvvvvvvvv Miscellaneous vvvvvvvvvvvvvvvvvvvvvvvvvv \n");
 		System.out.println(m.sayHello("Alex"));
 		m.play();
 		System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^ Miscellaneous ^^^^^^^^^^^^^^^^^^^^^^^^^^ \n");
 		
-    }
+	}
 
 	public String sayHello(String name) {
-		return name == null ? "Hello!" : "Hello, "+name+"!";
+ 		return name == null ? "Hello!" : "Hello, "+name+"!";
 	}
 
 	public void play(){
 		System.out.println("Play with Java.");
 
 	}
-
-	public void playHRAnagrams(){
-		System.out.println("Play with HackerRank Anagrams.");
-		String s = new String("abba");
-        java.util.Hashtable<String,Integer> ht = new java.util.Hashtable<String,Integer>(s.length());
-        for (int start = 0; start < s.length(); start++){
-            for (int end = start+1; end <= s.length(); end++){
-                String substr = s.substring(start,end);
-                String sig = m.getSig(substr);
-                Integer count = ht.get(sig);
-                if (count==null) count = new Integer(0);
-                count = new Integer(count.intValue() + 1);
-                ht.put(sig,count);
-            }
-        }
-        int anagrams=0;
-        //System.out.println(ht.keySet());
-        System.out.println(ht.values());
-        for (Integer count : ht.values()){
-            int cnt = count.intValue();
-            anagrams+=cnt*(cnt-1)/2;
-        }
-        System.out.println(anagrams);
-    }
-
-    private String getSig(String s){
-		int[] sig = new int[26];
-		String strSig = "";
-        for (int i = 0; i<s.length(); i++){
-            char c = s.charAt(i);
-            int index = c - 'a';
-            sig[index]++;
-		}
-		strSig = java.util.Arrays.toString(sig);
-        System.out.println(s);
-        System.out.println(strSig);
-        return strSig;
-    } 
 
 }
